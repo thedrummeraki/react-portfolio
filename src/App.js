ad473e89b36897ababd6377a5a8741db22e8d13f
Akinyele Cafe-Febrissy <akinyele.cafefebrissy@shopify.com>
2019-06-03 22:52:31 -0400

    Remove all React warnings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import {
-  Hero,
-} from 'bloomer';
 import 'bulma/css/bulma.css';
 import './App.css';
+
 import Welcome from './Welcome';
 import Credits from './Credits';
 
--- src/Credits.js
+++ src/Credits.js
@@ -18,7 +18,7 @@ export default function Credits(props) {
             <Column isFull>
               <p>
                 Made with<Icon hasTextColor="danger" className="fa fa-heart"></Icon>
-                by <a>Akinyele Cafe-Febrissy</a>
+                by <b>Akinyele Cafe-Febrissy</b>
               </p>
             </Column>
           </Columns>
@@ -35,7 +35,7 @@ export default function Credits(props) {
 function renderIconsCredits() {
   return (
     <div>
-      Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+      Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a>
     </div>
   );
 }
